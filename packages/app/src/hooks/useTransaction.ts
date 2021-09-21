ef42af03d9b768cc096d3754dc264d802d39dc51
root <rishabchirania>
2021-09-21 20:44:41 +0530

    main branch revert

--- packages/app/src/hooks/useTransaction.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { TransactionContext } from "../context/TransactionProvider";
-
-const useTransaction = () => {
-  const [transaction, dispatchTx] = useContext(TransactionContext);
-  return { transaction, dispatchTx };
-};
-
-export default useTransaction;
