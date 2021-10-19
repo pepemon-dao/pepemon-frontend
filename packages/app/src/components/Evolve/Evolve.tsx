e310945663de20d418f616544e46c3d6e896f451
cre-mer <jonasmerhej1@gmail.com>
2021-10-19 15:29:58 +0200

    optimize assets

--- packages/app/src/components/Evolve/Evolve.tsx
+++ packages/app/src/components/Evolve/Evolve.tsx
@@ -3,25 +3,25 @@ import styled from "styled-components";
 import { AnimatedImg } from "../../components";
 import {
 	// downgreenarrow,
-	pepemander, pemeleon, fudizard } from "../../assets";
+	pepertle, warpertle, rektoise } from "../../assets";
 
 const Evolve = () => {
 	return (
 		<div style={{ position: "relative", height: "100%", width: "250%", display: "grid", gridAutoRows: "1fr"}}>
 			<div>
 				<div style={{width: "28%"}}>
-					<img loading="lazy" src={pepemander} alt="pepemander" />
+					<AnimatedImg src={pepertle} alt="pepertle" />
 				</div>
 			</div>
 			<div style={{width: "100%", margin: "0 auto", transform: "translateY(-30%)"}}>
 				{/*<img loading="lazy" src={downgreenarrow} alt="downgreenarrow" style={{position: "relative", left: "40%", transform: "translateX(-50%) translateY(60%)"}}/>*/}
 				<DashedContainer style={{left: "50%", transform: "translateX(-50%)"}}>
-					<AnimatedImg src={pemeleon} alt="pemeleon" />
+					<AnimatedImg src={warpertle} alt="warpertle" />
 				</DashedContainer>
 			</div>
 			<div style={{width: "100%", margin: "0 0 0 auto", transform: "translateY(-60%)"}}>
 				<DashedContainer style={{ right: "0"}}>
-					<AnimatedImg src={fudizard} alt="fudizard" />
+					<AnimatedImg src={rektoise} alt="rektoise" />
 				</DashedContainer>
 			</div>
 		</div>
