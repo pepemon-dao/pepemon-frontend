770e2fae133c4c899064f5b04678969481a0e89a
cre-mer <jonasmerhej1@gmail.com>
2021-10-20 13:46:00 +0200

    remove underline from social boxes

--- packages/app/src/components/SocialBoxes/SocialBoxes.tsx
+++ packages/app/src/components/SocialBoxes/SocialBoxes.tsx
@@ -14,38 +14,38 @@ const SocialBoxes: React.FC<any> = () => {
 			<Text as="p" font={theme.font.spaceMace} align="center" underline>Our socials</Text>
 			<Spacer size="lg"/>
 			<ContentBoxGrid gridTemplate='"socialBox1 socialBox2 socialBox3 socialBox4"'>
-				<ContentBox as="a" href="https://twitter.com/pepemonfinance" target="_blank" rel="noopener noreferrer"
-				bgColor={theme.color.purple[300]} style={{ gridArea: "socialBox1", boxShadow: "0 18.7px 14.2px 0 rgba(0, 0, 0, 0.1)" }}>
+				<ContentBoxLink as="a" href="https://twitter.com/pepemonfinance" target="_blank" rel="noopener noreferrer"
+				bgColor={theme.color.purple[300]} gridArea="socialBox1">
 					<StyledSocialIcon loading="lazy" src={twitter} alt="twitter"/>
 					<Spacer size="sm"/>
 					<Text as="p" size={2} font={theme.font.neometric} weight={900} align="center">Twitter</Text>
 					<Spacer size="sm"/>
 					<Text as="p" align="center" lineHeight={1.5}>Follow us on Twitter for all updates and anouncements.</Text>
-				</ContentBox>
-				<ContentBox as="a" href="https://t.me/pepemonfinance" target="_blank" rel="noopener noreferrer"
-				bgColor={theme.color.purple[300]} style={{ gridArea: "socialBox2", boxShadow: "0 18.7px 14.2px 0 rgba(0, 0, 0, 0.1)" }}>
+				</ContentBoxLink>
+				<ContentBoxLink as="a" href="https://t.me/pepemonfinance" target="_blank" rel="noopener noreferrer"
+				bgColor={theme.color.purple[300]} gridArea="socialBox2">
 					<StyledSocialIcon loading="lazy" src={telegram} alt="telegram"/>
 					<Spacer size="sm"/>
 					<Text as="p" size={2} font={theme.font.neometric} weight={900} align="center">Telegram</Text>
 					<Spacer size="sm"/>
 					<Text as="p" align="center" lineHeight={1.5}>Join us on Telegram to ask us questions and talk with your fellow Pepemon trainers.</Text>
-				</ContentBox>
-				<ContentBox as="a" href="https://discord.com/invite/R8sZwMv" target="_blank" rel="noopener noreferrer"
-				bgColor={theme.color.purple[300]} style={{ gridArea: "socialBox3", boxShadow: "0 18.7px 14.2px 0 rgba(0, 0, 0, 0.1)" }}>
+				</ContentBoxLink>
+				<ContentBoxLink as="a" href="https://discord.com/invite/R8sZwMv" target="_blank" rel="noopener noreferrer"
+				bgColor={theme.color.purple[300]} gridArea="socialBox3">
 					<StyledSocialIcon loading="lazy" src={discord} alt="discord"/>
 					<Spacer size="sm"/>
 					<Text as="p" size={2} font={theme.font.neometric} weight={900} align="center">Discord</Text>
 					<Spacer size="sm"/>
 					<Text as="p" align="center" lineHeight={1.5}>Come hang out with us and all the Pepemon trainers on Discord.</Text>
-				</ContentBox>
-				<ContentBox as="a" href="https://pepemonfinance.medium.com/" target="_blank" rel="noopener noreferrer"
-				bgColor={theme.color.purple[300]} style={{ gridArea: "socialBox4", boxShadow: "0 18.7px 14.2px 0 rgba(0, 0, 0, 0.1)" }}>
+				</ContentBoxLink>
+				<ContentBoxLink as="a" href="https://pepemonfinance.medium.com/" target="_blank" rel="noopener noreferrer"
+				bgColor={theme.color.purple[300]} gridArea="socialBox4">
 					<StyledSocialIcon loading="lazy" src={medium} alt="medium"/>
 					<Spacer size="sm"/>
 					<Text as="p" size={2} font={theme.font.neometric} weight={900} align="center">Medium</Text>
 					<Spacer size="sm"/>
 					<Text as="p" align="center" lineHeight={1.5}>Find more detailed articles on Medium about Pepemon and the ecosystem.</Text>
-				</ContentBox>
+				</ContentBoxLink>
 			</ContentBoxGrid>
 		</ContentCentered>
 	)
@@ -55,6 +55,12 @@ const StyledSocialIcon = styled.img`
 	margin-left: auto;
 	margin-right: auto;
 	width: 3.75em;
+`
+
+const ContentBoxLink = styled(ContentBox)<{gridArea: string}>`
+	box-shadow: 0 18.7px 14.2px 0 rgba(0, 0, 0, 0.1);
+	grid-area: ${props => props.gridArea};
+	text-decoration: none;
 `
 
 export default SocialBoxes;
