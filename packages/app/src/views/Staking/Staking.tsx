9832a362aa584cbc24140524e16ea3663764b70f
Pepemon <72664737+pepem00n@users.noreply.github.com>
2022-06-19 23:21:31 +0200

    new-staking-copy

--- packages/app/src/views/Staking/Staking.tsx
+++ packages/app/src/views/Staking/Staking.tsx
@@ -3,8 +3,8 @@ import { DefaultPage } from '../../components';
 import StakeCard from './components/StakeCard';
 
 export const stakingMeta = {
-	title: 'Pepemon - Staking',
-	description: 'Users have the choice of staking PPBLZ on our platform for basic APY, or stake PPBLZ-ETH LP tokens for double the rewards in PPDEX. Collect fees and help other Pepetrainers trade by staking LP tokens.'
+	title: 'Pepemon! Earn',
+	description: 'Users have the choice of earning with PPBLZ on our platform for a basic APY, or earn with PPBLZ-ETH LP tokens for double the rewards in PPDEX. Collect fees and help other Pepemon trainers get in the game.'
 }
 
 const Staking: React.FC<any> = () => {
