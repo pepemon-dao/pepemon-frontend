e8593fbb685edab1682661160c02eb7def72e259
Pepemon <72664737+pepem00n@users.noreply.github.com>
2023-05-10 12:57:43 +0200

    Update Bridge.tsx

--- packages/app/src/views/Bridge/Bridge.tsx
+++ packages/app/src/views/Bridge/Bridge.tsx
@@ -3,13 +3,13 @@ import { DefaultPage } from '../../components';
 import BridgeCard from './components/BridgeCard';
 
 export const bridgeMeta = {
-	title: 'Bridge your coins to the Pepe Chain',
-	description: "Bridge your $ppdex and other pepemon tokens to the Pepemon L2 chain. You'll be able to use them in the game and earn rewards for doing so."
+	title: 'Bridge your coins to the Pepechain L2',
+	description: "Bridge your $PPDEX and other pepemon tokens to the Pepemon L2 chain. You'll be able to use them in the game and earn rewards for doing so."
 }
 
 const Bridge: React.FC<any> = () => {
 	return (
-		<DefaultPage title='BridgeCard'>
+		<DefaultPage title='Bridge to Pepechain L2'>
 			<BridgeCard/>
 		</DefaultPage>
 	)
