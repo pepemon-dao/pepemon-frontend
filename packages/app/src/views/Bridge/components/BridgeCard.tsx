f27c55e4c2e7515a3157baea730315b1ecfa588c
Drikus Roor <drikusroor@gmail.com>
2023-05-14 15:03:34 +0200

    Implement testnet dapp iframes

--- packages/app/src/views/Bridge/components/BridgeCard.tsx
+++ packages/app/src/views/Bridge/components/BridgeCard.tsx
@@ -1,30 +1,27 @@
-import React, { useContext } from "react";
+import React from "react";
 import styled from "styled-components";
-import { PepemonProviderContext } from "../../../contexts";
 import { Badge, StyledLinkTitle } from "../../../components";
 import {
   StyledStoreBody,
   StyledStoreHeader,
   StyledStoreWrapper,
 } from "../../Store/components";
-import { getPpblzAddress, getPpdexAddress } from "../../../pepemon/utils";
 import { Link, Redirect, useParams } from "react-router-dom";
 import BridgeSubview from "./BridgeSubview";
 
 const BridgeCard: React.FC<any> = () => {
-
   const routerParams: any = useParams();
 
-  console.log(routerParams.bridgeState)
-
-  if (!routerParams.bridgeState) return <Redirect to="/bridge/testnet"/>
+  if (!routerParams.bridgeState) return <Redirect to="/bridge/testnet" />;
 
   return (
     <div>
       <StyledStoreWrapper>
         <StyledStoreHeader>
           <div style={{ display: "flex" }}>
-            <StyledLinkTitle isInactive={routerParams.bridgeState !== "testnet"}>
+            <StyledLinkTitle
+              isInactive={routerParams.bridgeState !== "testnet"}
+            >
               <Link to={`/bridge/testnet`}>Bridge</Link>
             </StyledLinkTitle>
             <StyledLinkTitle
@@ -48,23 +45,33 @@ const BridgeCard: React.FC<any> = () => {
           </div>
         </StyledStoreHeader>
         <StyledStoreBody>
-          {routerParams.bridgeState === "testnet" && (
-            <BridgeSubview />
-          )}
+          {routerParams.bridgeState === "testnet" && <BridgeSubview />}
           {routerParams.bridgeState === "claim-ppblz" && (
-            <>
-              <h1>Claim PPBLZ</h1>
-            </>
+            <iframe
+              src="https://ipfs.thirdwebcdn.com/ipfs/QmfK9mw9eQKE9vCbtZht9kygpkNWffdwibsJPnCo7MBN4M/erc20.html?contract=0xD5678bCB3652a118A0B7e93C5C457e42ce263640&chain=%7B%22name%22%3A%22pepechain-testnet%22%2C%22chain%22%3A%22%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fl2-pepechain-testnet-8uk55qlld4.t.conduit.xyz%22%5D%2C%22nativeCurrency%22%3A%7B%22symbol%22%3A%22pepETH%22%2C%22name%22%3A%22pepETH%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22pepechain-testnet%22%2C%22chainId%22%3A906090%2C%22testnet%22%3Atrue%2C%22slug%22%3A%22pepechain-testnet%22%7D"
+              width="600px"
+              height="600px"
+              style={{ maxWidth: "100%" }}
+              frameBorder="0"
+            ></iframe>
           )}
           {routerParams.bridgeState === "mint-pepemon-avatars" && (
-            <>
-              <h1>Mint Pepemon Avatars</h1>
-            </>
+            <iframe
+              src="https://ipfs.thirdwebcdn.com/ipfs/QmfK9mw9eQKE9vCbtZht9kygpkNWffdwibsJPnCo7MBN4M/marketplace-v3.html?contract=0x2FCb67128BE211D7c94dA8990AC61E6af2Ea0BD0&chain=%7B%22name%22%3A%22pepechain-testnet%22%2C%22chain%22%3A%22%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fl2-pepechain-testnet-8uk55qlld4.t.conduit.xyz%22%5D%2C%22nativeCurrency%22%3A%7B%22symbol%22%3A%22pepETH%22%2C%22name%22%3A%22pepETH%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22pepechain-testnet%22%2C%22chainId%22%3A906090%2C%22testnet%22%3Atrue%2C%22slug%22%3A%22pepechain-testnet%22%7D&directListingId=0&primaryColor=red&secondaryColor=red"
+              width="600px"
+              height="600px"
+              style={{ maxWidth: "100%" }}
+              frameBorder="0"
+            ></iframe>
           )}
           {routerParams.bridgeState === "bid-on-pepesea" && (
-            <>
-              <h1>Bid on Pepesea</h1>
-            </>
+            <iframe
+              src="https://ipfs.thirdwebcdn.com/ipfs/QmfK9mw9eQKE9vCbtZht9kygpkNWffdwibsJPnCo7MBN4M/marketplace-v3.html?contract=0x2FCb67128BE211D7c94dA8990AC61E6af2Ea0BD0&chain=%7B%22name%22%3A%22pepechain-testnet%22%2C%22chain%22%3A%22%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fl2-pepechain-testnet-8uk55qlld4.t.conduit.xyz%22%5D%2C%22nativeCurrency%22%3A%7B%22symbol%22%3A%22pepETH%22%2C%22name%22%3A%22pepETH%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22pepechain-testnet%22%2C%22chainId%22%3A906090%2C%22testnet%22%3Atrue%2C%22slug%22%3A%22pepechain-testnet%22%7D&englishAuctionId=1&theme=system"
+              width="600px"
+              height="600px"
+              style={{ maxWidth: "100%" }}
+              frameBorder="0"
+            ></iframe>
           )}
         </StyledStoreBody>
       </StyledStoreWrapper>
