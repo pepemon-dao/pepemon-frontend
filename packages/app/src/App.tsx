9e5022fb76ae806e0b20f2e56745f76af4a06e9c
pepemonfinance@gmail.com <Pepemon@1>
2023-05-15 15:12:30 +0200

    fix: correct tab link for nft mint

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -57,7 +57,7 @@ const App: React.FC = () => {
             <Route path="/staking">
               <StakingWithAuth />
             </Route>
-            <Route path="/bridge/:bridgeState(testnet|claim-ppblz|mint-pepemon-avatars|bid-on-pepesea)?">
+            <Route path="/bridge/:bridgeState(testnet|claim-ppblz|mint-pepemon-badges|bid-on-pepesea)?">
               <BridgeWithAuth />
             </Route>
             <Route path="/subscription">
