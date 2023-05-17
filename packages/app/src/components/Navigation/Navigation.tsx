0e1606e7634f79c9a30272ed2f382f6b9cd0f220
pepemonfinance@gmail.com <Pepemon@1>
2023-05-17 19:16:15 +0200

    fix: portal instead of bridge in navbar

--- packages/app/src/components/Navigation/Navigation.tsx
+++ packages/app/src/components/Navigation/Navigation.tsx
@@ -90,7 +90,7 @@ const Navigation = () => {
           >
             <StyledLink to="/bridge">
               <StyledLinkIcon loading="lazy" src={bridge} alt="bridge" />
-              <span>Bridge</span>
+              <span>Portal</span>
             </StyledLink>
           </StyledMenuListItem>
           <StyledMenuListItem soon isActive={false}>
